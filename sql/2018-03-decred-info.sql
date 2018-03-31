139ea40aeb9fb73235ecf0dbbc7b8728114d95d9
Greg Padin <padin.greg@gmail.com>
2018-03-31 09:36:11 -0400

    adding Hcash assets

---
+++ sql/2018-03-decred-info.sql
@@ -0,0 +1,9 @@
+-- Recent additions to add after db init (.gz)
+-- mysql yaamp -p < file.sql
+
+-- Added details to the decred config
+
+
+UPDATE coins SET algo='blake256', image= '/images/coin-Decred.png' WHERE name = 'decred';
+
+
