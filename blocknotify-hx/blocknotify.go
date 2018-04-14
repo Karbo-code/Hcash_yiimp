ea1f640faee38d74a278aa6b6fbbfd1654463b89
Ubuntu <ubuntu@ip-172-31-20-212.us-west-2.compute.internal>
2018-04-14 13:57:27 -0400

    Uploading current server state

--- blocknotify-hx/blocknotify.go
+++ blocknotify-hx/blocknotify.go
@@ -22,7 +22,7 @@ import (
 
 const (
 	processName = "/var/stratum/blocknotify"    // set the full path if required
-	stratumDest = "54.202.49.38:3252" // stratum host:port TO RUN DECRED AND STRATUM AT SAME TIME NEED TO CREATE HX.CONF FILE AND ASSIGN HX A NEW PORT
+	stratumDest = "34.216.11.82:3252" // stratum host:port TO RUN DECRED AND STRATUM AT SAME TIME NEED TO CREATE HX.CONF FILE AND ASSIGN HX A NEW PORT
 	coinId = "1307"                // decred database coin id
 
 
